e7f2d2ff9e21bdde17e2d6875facbad34bd5c2d0
runJW <>
2021-05-12 07:57:16 +0000

    4.2 Dynamic URLS and templates function added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("Flask_Hello")
 
@@ -6,8 +6,8 @@ app = Flask("Flask_Hello")
 def home():
   return "Hello! Welcome to mi casa!"
 
-@app.route("/contact")
-def potato():
-  return "Contact me!"
+@app.route("/<username>")
+def potato(username):
+  return render_template("potato.html")
 
 app.run(host="0.0.0.0")
