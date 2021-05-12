898e236c239169d82c17b6300cdbe66ce749aead
runJW <>
2021-05-12 15:38:29 +0000

    4.5 Apply Faster Scrapper

--- main.py
+++ main.py
@@ -3,19 +3,30 @@ from scrapper import get_jobs
 
 app = Flask("Flask_Hello")
 
+db = {}
+
+
 @app.route("/")
 def home():
-  return render_template("potato.html")
+    return render_template("potato.html")
+
 
 @app.route("/report")
 def report():
-  word = request.args.get('word')
-  if word:
-    word = word.lower()
-    jobs = get_jobs(word)
-    print(jobs)
-  else:
-    return redirect("/")
-  return render_template("report.html", searchingBy=word, potato="awesome")
+    word = request.args.get('word')
+    if word:
+        word = word.lower()
+        fromDb = db.get(word)
+        if fromDb:
+            jobs = fromDb
+        else:
+            jobs = get_jobs(word)
+            db[word] = jobs
+    else:
+        return redirect("/")
+    return render_template("report.html",
+                           searchingBy=word,
+                           resultsNumber=len(jobs))
+
 
 app.run(host="0.0.0.0")
