b5d2e150e54d50c48ff89d960480917883947e99
runJW <>
2021-05-12 08:10:32 +0000

    4.3 Forms and Query Arguments Added

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask("Flask_Hello")
 
 @app.route("/")
 def home():
-  return "Hello! Welcome to mi casa!"
-
-@app.route("/<username>")
-def potato(username):
   return render_template("potato.html")
+
+@app.route("/report")
+def report():
+  word = request.args.get('word')
+  return render_template("report.html", searchingBy=word, potato="awesome")
 
 app.run(host="0.0.0.0")
